cf6c3afe564fb4d6bc4c85ca3e54239bf6957681
maz <123456@qq.com>
2019-04-15 15:09:54 +0800

    第三次

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,9 @@ const server = require("gulp-webserver");
 const uglify = require("gulp-uglify");
 const img = require("gulp-imagemin");
 const sass = require("gulp-sass");
+const fs = require("fs");
+const path = require("path");
+const url = require("url");
 
 //压缩css
 gulp.task("css", () => {
@@ -25,5 +28,19 @@ gulp.task("server", () => {
             port: 8080,
             livereload: true,
             open: true,
+            middleware: (req, res) => {
+                let filepath = url.parse(req.url).pathname;
+                if (req.url === "/favicon.ico") return;
+                if (filepath === "/") {
+                    let path1 = fs.readFileSync(path.join(__dirname, "./src/index.html"))
+                    res.end(path1)
+                } else if (filepath === "/api") {
+                    let data = fs.readFileSync(path.join(__dirname, "./src/data/data.json"))
+                    res.end(data);
+                } else {
+                    let file1 = fs.readFileSync(path.join(__dirname, "src", filepath))
+                    res.end(file1);
+                }
+            }
         }))
 })
